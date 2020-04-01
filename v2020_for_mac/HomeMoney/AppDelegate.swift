a7618b853793ef810be1e6d05fec76ab5b8ab465
Evgenii Sopov <mrseakg@gmail.com>
2020-04-01 14:11:13 +0700

    Init project for mac

---
+++ v2020_for_mac/HomeMoney/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HomeMoney
+//
+//  Created by sea-kg on 01.04.2020.
+//  Copyright © 2020 sea-kg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
